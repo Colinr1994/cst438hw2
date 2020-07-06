a4fc721406d87fe01f5c6be877d6eaaa4813b655
Colinr1994 <colinr1994@gmail.com>
2020-07-06 13:24:45 -0700

    getTempF complete

--- src/main/java/cst438hw2/domain/TempAndTime.java
+++ src/main/java/cst438hw2/domain/TempAndTime.java
@@ -15,10 +15,17 @@ public class TempAndTime {
 	
 	public double getTemp() { return temp; }
 	public void setTemp(double temp) { this.temp = temp; }
+	public double getTempF() {
+		Double tempF = (temp-273.15) * (9.0/5.0) + 32;
+		DecimalFormat df = new DecimalFormat("#.##");
+		return Double.valueOf(df.format(tempF));
+	}
 	
 	public long getTime() { return time; }
 	public void setTime(long time) { this.time = time; } 
 	
+	
 	public int getTimezone() { return timezone; } 
 	public void seTimezone(int timezone) { this.timezone = timezone;}
+	
  }
