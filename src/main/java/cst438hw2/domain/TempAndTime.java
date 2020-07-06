a31184dddfec2cac645fb60422573ce039d449df
Colinr1994 <colinr1994@gmail.com>
2020-07-06 13:38:45 -0700

    TempAndTime complete?

--- src/main/java/cst438hw2/domain/TempAndTime.java
+++ src/main/java/cst438hw2/domain/TempAndTime.java
@@ -1,6 +1,8 @@
 package cst438hw2.domain;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TempAndTime {
 	public double temp;
@@ -15,17 +17,25 @@ public class TempAndTime {
 	
 	public double getTemp() { return temp; }
 	public void setTemp(double temp) { this.temp = temp; }
-	public double getTempF() {
+	
+	public double getFormTemp() {
 		Double tempF = (temp-273.15) * (9.0/5.0) + 32;
 		DecimalFormat df = new DecimalFormat("#.##");
 		return Double.valueOf(df.format(tempF));
 	}
 	
 	public long getTime() { return time; }
-	public void setTime(long time) { this.time = time; } 
+	public void setTime(long time) { this.time = time; }
+	
+	public String getFormTime() {
+	    SimpleDateFormat formatter = new SimpleDateFormat("hh:mm a");
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.setTimeInMillis(time * 1000);
+	    return formatter.format(calendar.getTime());
+	}
 	
 	
 	public int getTimezone() { return timezone; } 
-	public void seTimezone(int timezone) { this.timezone = timezone;}
+	public void seTimezone(int timezone) { this.timezone = timezone; }
 	
  }
